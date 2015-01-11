ee34b46566297a4a34fdceab0fb4766adc142c8c
Tsvetomil <cvetomill@gmail.com>
2015-01-11 11:19:25 +0200

    add no image

--- app/js/services/adsData.js
+++ app/js/services/adsData.js
@@ -5,7 +5,7 @@ adsApp.factory('adsDataService', function ($http, $log) {
             var pageIndex = 1;
             $http({
                 method: 'GET',
-                url: 'http://softuni-ads.azurewebsites.net/api/ads?pagesize=3&startpage=1' + pageIndex})
+                url: 'http://softuni-ads.azurewebsites.net/api/ads?pagesize=50&startpage=' + pageIndex}) //?pagesize=3&startpage=1
                 .success(function (data, status, headers, config) {
                     success(data);
                 })
